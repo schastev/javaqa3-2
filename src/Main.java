d455642ce3224119cb14500b154914309e14e27e
k-emiko <62057364+k-emiko@users.noreply.github.com>
2021-03-11 22:47:40 +1100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100;
+        int add = Integer.parseInt(args[0]); //to use this with a CL argument instead of changing the variable and recompiling
+        int bonus = 0;
+        if (add > 1000) {
+            bonus = add / 100;
+        } else {
+            account = account + add;
+        }
+        //account = account + add + bonus; //kinda irrelevant
+        System.out.println(bonus);
+    }
+
+
+}
